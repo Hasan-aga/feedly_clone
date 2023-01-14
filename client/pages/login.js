210ebbd6545e9b5dd317b718dd47c7f4e574b418
Hasan-aga <careerforma@gmail.com>
2023-01-14 23:07:10 +0300

    add login page

--- client/pages/login.js
+++ client/pages/login.js
@@ -11,20 +11,22 @@ import {
 export default function Login() {
   return (
     <Grid.Container
-      gap={2}
-      justify="center"
-      alignItems="center"
       direction="column"
+      alignItems="center"
+      justify="center"
+      css={{ height: "100vh" }}
     >
-      <Grid>
-        <Input placeholder="Email" />
-      </Grid>
-      <Grid>
-        <Input placeholder="Password" />
-      </Grid>
-      <Grid>
-        <Button>Signup / Login</Button>
-      </Grid>
+      <Grid.Container gap={2} alignItems="center" direction="column" xs={6}>
+        <Grid>
+          <Input placeholder="Email" />
+        </Grid>
+        <Grid>
+          <Input placeholder="Password" />
+        </Grid>
+        <Grid>
+          <Button>Signup / Login</Button>
+        </Grid>
+      </Grid.Container>
     </Grid.Container>
   );
 }
