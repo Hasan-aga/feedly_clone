2659b06815ba96bb489c10fde577dc5ceddab608
Hasan-aga <careerforma@gmail.com>
2023-01-15 18:22:43 +0300

    implement 0auth github

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,13 +1,16 @@
 import * as React from "react";
+import { SessionProvider } from "next-auth/react";
 
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from "@nextui-org/react";
 
-export default function App({ Component }) {
+export default function App({ Component, session }) {
   // 2. Use at the root of your app
   return (
     <NextUIProvider>
-      <Component />
+      <SessionProvider session={session}>
+        <Component />
+      </SessionProvider>
     </NextUIProvider>
   );
 }
