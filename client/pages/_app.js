6ea11d5810197f1fca2e29839532ac13aa3f9eeb
Hasan-aga <careerforma@gmail.com>
2023-01-27 18:21:09 +0300

    add themes

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,16 +1,34 @@
 import * as React from "react";
 import { SessionProvider } from "next-auth/react";
-
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 // 1. import `NextUIProvider` component
-import { NextUIProvider } from "@nextui-org/react";
 
 export default function App({ Component, session, pageProps }) {
   // 2. Use at the root of your app
+  const lightTheme = createTheme({
+    type: "light",
+    theme: {},
+  });
+
+  const darkTheme = createTheme({
+    type: "dark",
+    theme: {},
+  });
   return (
-    <NextUIProvider>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </NextUIProvider>
+    <NextThemesProvider
+      defaultTheme="dark"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
+      <NextUIProvider>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </NextUIProvider>
+    </NextThemesProvider>
   );
 }
--- client/pages/profile.js
+++ client/pages/profile.js
@@ -1,16 +1,34 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import { User, Grid, Button, Card, Text, Spacer } from "@nextui-org/react";
+import {
+  User,
+  Grid,
+  Button,
+  Spacer,
+  Switch,
+  useTheme,
+} from "@nextui-org/react";
 import CustomModal from "@/pages/modal";
 import { useState } from "react";
 import ContentStack from "@/components/contentStack";
+import { useTheme as useNextTheme } from "next-themes";
 
 export default function Profile() {
   const { data: session } = useSession();
   const [visible, setVisible] = useState(false);
 
+  const { setTheme } = useNextTheme();
+  const { isDark, type } = useTheme();
+
   if (session) {
     return (
       <>
+        <div>
+          The current theme is: {type}
+          <Switch
+            checked={isDark}
+            onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
+          />
+        </div>
         <User src={session.user.image} name={session.user.name} />
         <br />
         <Button onPress={() => signOut()}>Sign out</Button>
