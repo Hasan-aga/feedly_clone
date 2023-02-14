cb3c47d98b5063cdb3189cc9f06030440c409ea4
Hasan-aga <careerforma@gmail.com>
2023-02-14 10:04:54 +0300

    render msg for missing categroy

--- client/pages/categories/[category].js
+++ client/pages/categories/[category].js
@@ -2,24 +2,26 @@ import useFeeds from "@/hooks/useFeeds";
 import { useRouter } from "next/router";
 import ContentStack from "@/components/contentStack";
 import Feed from "@/components/feed";
-import { Collapse } from "@nextui-org/react";
+import { Collapse, Loading, Text } from "@nextui-org/react";
+import { isError } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
 
 export default function SpecificCategory() {
   const router = useRouter();
   const { title } = router.query;
-  const {
-    data: { results },
-  } = useFeeds();
-  if (results) {
-    console.log(results[title][0].title);
+  const { data, isLoading } = useFeeds();
 
-    return (
+  if (isLoading) return <Loading type="points" />;
+  if (data) {
+    console.log("data", data);
+    const { results } = data;
+    return results[title] ? (
       <Collapse.Group>
-        {results[title].map((feed, key) => (
-          <Feed feed={feed} key={key} />
-        ))}
+        {results[title] &&
+          results[title].map((feed, key) => <Feed feed={feed} key={key} />)}
       </Collapse.Group>
+    ) : (
+      <Text color="warning">No results for this category.</Text>
     );
   }
-  return <p>loading...</p>;
 }
