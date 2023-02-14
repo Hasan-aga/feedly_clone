cb8f1ae9797241494d58208cb3682e626a24ffc6
Hasan-aga <careerforma@gmail.com>
2023-02-14 15:56:42 +0300

    abandon position sticky

--- client/components/sidebar.js
+++ client/components/sidebar.js
@@ -8,23 +8,16 @@ export default function Sidebar({ session, feeds }) {
   const [visible, setVisible] = useState(false);
 
   return (
-    <div style={{ position: "sticky", top: "0" }}>
-      <Grid.Container
-        xs={12}
-        direction="column"
-        gap={2}
-        alignItems="flex-start"
-      >
-        <ProfileMenu session={session} />
-        <Grid>
-          <Spacer y={10} />
-          <Button size="sm" onPress={() => setVisible(true)}>
-            <Text b>Add a feed +</Text>
-          </Button>
-        </Grid>
-        <CustomModal visible={visible} closeHandler={() => setVisible(false)} />
-        <Categories feeds={feeds} />
-      </Grid.Container>
-    </div>
+    <Grid.Container xs={12} gap={1} alignItems="center" justify="flex-start">
+      <ProfileMenu session={session} />
+      <Grid>
+        <Spacer y={2} />
+        <Button size="sm" onPress={() => setVisible(true)}>
+          <Text b>Add a feed +</Text>
+        </Button>
+      </Grid>
+      <CustomModal visible={visible} closeHandler={() => setVisible(false)} />
+      <Categories feeds={feeds} />
+    </Grid.Container>
   );
 }
