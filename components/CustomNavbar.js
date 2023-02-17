36622ec33927271c0702ca3a08a7829cbdb14230
Hasan-aga <careerforma@gmail.com>
2023-02-17 20:00:46 +0300

    add "add feed" to navabar

--- components/CustomNavbar.js
+++ components/CustomNavbar.js
@@ -1,45 +1,59 @@
+import CustomModal from "@/pages/modal";
 import { Button, Navbar, Text, Tooltip } from "@nextui-org/react";
 import Link from "next/link";
+import { useState } from "react";
+import Add from "./icons/add";
 import Bookmark from "./icons/bookmark";
 import Cog from "./icons/cog";
 import HomeIcon from "./icons/homeIcon";
 import ProfileMenu from "./profileMenu";
 
 export default function CustomNavbar({ session }) {
-  return (
-    <Navbar isCompact isBordered variant="sticky">
-      <Navbar.Content>
-        <ProfileMenu session={session} />
-      </Navbar.Content>
-      <Navbar.Content variant="underline">
-        <Navbar.Item>
-          <Link href="/">
-            <Tooltip placement="bottom" color="primary" content="Home">
-              <HomeIcon />
-            </Tooltip>
-          </Link>
-        </Navbar.Item>
-        <Navbar.Item>
-          <Link href="/settings/">
-            <Tooltip placement="bottom" color="primary" content="Settings">
-              <Cog />
-            </Tooltip>
-          </Link>
-        </Navbar.Item>
-        <Navbar.Item>
-          <Link href="/bookmarks">
-            <Tooltip placement="bottom" color="primary" content="Bookmarks">
-              <Bookmark />
-            </Tooltip>
-          </Link>
-        </Navbar.Item>
-      </Navbar.Content>
+  const [visible, setVisible] = useState(false);
 
-      <Navbar.Brand hideIn="xs">
-        <Text b color="inherit">
-          Feedni
-        </Text>
-      </Navbar.Brand>
-    </Navbar>
+  return (
+    <div>
+      <Navbar isCompact isBordered variant="sticky">
+        <Navbar.Content>
+          <ProfileMenu session={session} />
+        </Navbar.Content>
+        <Navbar.Content variant="underline">
+          <Navbar.Item>
+            <Link href="/">
+              <Tooltip placement="bottom" color="primary" content="Home">
+                <HomeIcon />
+              </Tooltip>
+            </Link>
+          </Navbar.Item>
+          <Navbar.Item>
+            <div onClick={() => setVisible(true)}>
+              <Tooltip placement="bottom" color="primary" content="Add">
+                <Add />
+              </Tooltip>
+            </div>
+          </Navbar.Item>
+          <Navbar.Item>
+            <Link href="/settings/">
+              <Tooltip placement="bottom" color="primary" content="Settings">
+                <Cog />
+              </Tooltip>
+            </Link>
+          </Navbar.Item>
+          <Navbar.Item>
+            <Link href="/bookmarks">
+              <Tooltip placement="bottom" color="primary" content="Bookmarks">
+                <Bookmark />
+              </Tooltip>
+            </Link>
+          </Navbar.Item>
+        </Navbar.Content>
+        <Navbar.Brand hideIn="xs">
+          <Text b color="inherit">
+            Feedni
+          </Text>
+        </Navbar.Brand>
+      </Navbar>
+      <CustomModal visible={visible} closeHandler={() => setVisible(false)} />
+    </div>
   );
 }
