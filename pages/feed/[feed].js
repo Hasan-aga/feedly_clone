4ecb827473aba6f742079385e4d8446e59b7482d
Hasan-aga <careerforma@gmail.com>
2023-02-18 15:13:59 +0300

    fix feed view page

--- pages/feed/[feed].js
+++ pages/feed/[feed].js
@@ -1,18 +1,31 @@
 import Feed from "@/components/feed";
+import useFeeds from "@/hooks/useFeeds";
 import { Collapse, Grid } from "@nextui-org/react";
 import { useRouter } from "next/router";
+
+function getMyFeedFromCategories(categories, feedTitle) {
+  // todo: this process is reversed in categories.
+  let totalFeeds = [];
+  for (const [category, feeds] of Object.entries(categories)) {
+    for (const feed of feeds) {
+      if (feed.title == feedTitle) return feed;
+    }
+  }
+  return totalFeeds;
+}
 
 export default function SpecificFeed() {
   const router = useRouter();
   const feed = router.query;
 
-  return (
-    <>
-      <Grid xs={10} justify="center">
+  const { data, isFetching } = useFeeds();
+
+  if (data)
+    return (
+      <div style={{ width: "100%" }}>
         <Collapse.Group>
-          <Feed feed={feed} />
+          {<Feed feed={getMyFeedFromCategories(data.results, feed.title)} />}
         </Collapse.Group>
-      </Grid>
-    </>
-  );
+      </div>
+    );
 }
