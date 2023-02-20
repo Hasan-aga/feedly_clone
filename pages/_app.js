2b9a832866aa88c4c3f086fc4fce298859985c13
Hasan-aga <careerforma@gmail.com>
2023-02-20 19:05:00 +0300

    remove useless comment

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import "./_app.style.css";
 export default function App({ Component, session, pageProps }) {
-  // 2. Use at the root of your app
   const lightTheme = createTheme({
     type: "light",
     theme: {
@@ -44,9 +43,6 @@ export default function App({ Component, session, pageProps }) {
       },
     },
   });
-
-  //todo: override default themes
-  // if you remove NextUIProvider, themes still work since we are using default themes.
 
   //react query
   const [queryClient] = React.useState(() => new QueryClient());
