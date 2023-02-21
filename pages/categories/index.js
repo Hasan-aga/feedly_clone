6969aa848cccdedb6e769fe4145e01b1a2a3e9ed
Hasan-aga <careerforma@gmail.com>
2023-02-21 08:41:22 +0300

    implement categories page

---
+++ pages/categories/index.js
@@ -0,0 +1,10 @@
+import Categories from "@/components/categories";
+import { Container } from "@nextui-org/react";
+
+export default function CategoriesPage(params) {
+  return (
+    <Container xs={12}>
+      <Categories />
+    </Container>
+  );
+}
