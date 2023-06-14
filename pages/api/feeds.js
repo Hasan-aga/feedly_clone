849e7c1f34fec3fc444ce558e7ca2d953bea693c
Hasan-aga <careerforma@gmail.com>
2023-06-14 22:24:04 +0300

    delay updating with timeout

--- pages/api/feeds.js
+++ pages/api/feeds.js
@@ -21,6 +21,7 @@ export default async function handler(req, res) {
       case "GET":
         {
           const results = await controller.getMyFeeds();
+          console.log("returned from api");
           res.status(200).json({ success: true, results });
         }
         break;
