2fe281a145f8a489707e8040a1dbc18e56abafc5
Hasan-aga <careerforma@gmail.com>
2023-06-15 22:15:27 +0300

    call update after res.send

--- pages/api/feeds.js
+++ pages/api/feeds.js
@@ -20,9 +20,9 @@ export default async function handler(req, res) {
         break;
       case "GET":
         {
-          const results = await controller.getMyFeeds();
-          controller.checkAndUpdate(results);
-          res.status(200).json({ success: true, results });
+          const { results, groupedResults } = await controller.getMyFeeds();
+          res.status(200).json({ success: true, results: groupedResults });
+          await controller.checkAndUpdate(results);
         }
         break;
       case "DELETE":
